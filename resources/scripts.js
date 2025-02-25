c26297e5d45e0da2e48ac068bf06a34814e948c1
Martijn Nieuwenburg <martijnnieuwenburg@Martijns-MacBook-Pro.local>
2025-02-25 12:20:02 +0100

    toggler component

--- resources/scripts.js
+++ resources/scripts.js
@@ -5,6 +5,7 @@ import './styles.scss';
 window.Alpine = Alpine 
 
 // ===== Auto-Generated Imports =====
+import '@components/assignment_toggler/assignment_toggler.js';
 import '@components/junior_opdracht_1/junior_opdracht_1.js';
 import '@components/junior_opdracht_2/junior_opdracht_2.js';
 import '@components/test/test.js';
