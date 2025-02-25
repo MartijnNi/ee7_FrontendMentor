c9d1946a03c386b15b52187481b9318b9976ce04
Martijn Nieuwenburg <martijnnieuwenburg@Martijns-MacBook-Pro.local>
2025-02-25 13:32:21 +0100

    toggler + tips tab

--- resources/scripts.js
+++ resources/scripts.js
@@ -8,7 +8,6 @@ window.Alpine = Alpine
 import '@components/assignment_toggler/assignment_toggler.js';
 import '@components/junior_opdracht_1/junior_opdracht_1.js';
 import '@components/junior_opdracht_2/junior_opdracht_2.js';
-import '@components/test/test.js';
 import '@components/tips/tips.js';
 // ===== End Auto-Generated Imports =====
 
