c8aaa6890b51d38cb0cc20de7abeb4664477b701
Martijn Nieuwenburg <martijnnieuwenburg@Martijns-MacBook-Pro.local>
2025-03-04 10:38:46 +0100

    setup

--- resources/scripts.js
+++ resources/scripts.js
@@ -8,7 +8,7 @@ window.Alpine = Alpine
 import '@components/assignment_toggler/assignment_toggler.js';
 import '@components/junior_opdracht_1/junior_opdracht_1.js';
 import '@components/junior_opdracht_2/junior_opdracht_2.js';
-import '@components/newsletter/newsletter.js';
+import '@components/junior_opdracht_3/junior_opdracht_3.js';
 import '@components/tips/tips.js';
 // ===== End Auto-Generated Imports =====
 
